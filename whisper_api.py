57822f226b78837a2eb4ebc2637daeb93159029b
abdulkadir <>
2025-04-11 13:40:20 +0300

    add configuration files for deployment and update start script port

--- whisper_api.py
+++ whisper_api.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, File, UploadFile
 import whisper
 import tempfile
 import ssl
+import os
+
 ssl._create_default_https_context = ssl._create_unverified_context
 
 app = FastAPI()
@@ -16,5 +18,7 @@ async def transcribe(file: UploadFile = File(...)):
     with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as tmp:
         tmp.write(await file.read())
         path = tmp.name
+        print("Received file:", path)
+        print("File size:", os.path.getsize(path))
     result = model.transcribe(path, language="arabic")
     return {"text": result["text"]}
